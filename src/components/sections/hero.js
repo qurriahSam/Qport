be6611771e0a36966cd9a8e2f785929d84419044
qurriahSam <mugirethe1@gmail.com>
2024-04-06 00:27:49 +0300

    feat: update hero

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -3,6 +3,7 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styled from 'styled-components';
 import { navDelay, loaderDelay } from '@utils';
 import { usePrefersReducedMotion } from '@hooks';
+import { email } from '@config';
 
 const StyledHeroSection = styled.section`
   ${({ theme }) => theme.mixins.flexCenter};
@@ -71,11 +72,7 @@ const Hero = () => {
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
+    <a className="email-link" href={`mailto:${email}`} target="_blank" rel="noreferrer">
       Get In Touch
     </a>
   );
